a3933afe4e1aca8cffd6ce5a16035e8727699f6f
Alfred Ray Jayag <jayagalray@gmail.com>
2020-11-02 17:52:40 +0400

    setup update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url=URL,
     python_requires='>=3.6',
-    packages=setuptools.find_packages(where='src'),
+    packages=setuptools.find_packages(where='src', include=['zkafka']),
     install_requires=REQUIRED,
     include_package_data=True,
     license='MIT',
