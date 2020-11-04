6e3025cfef2f5c673a33b0837586b153a7f86536
Alfred Ray Jayag <jayagalray@gmail.com>
2020-11-04 12:04:36 +0400

    wait print update

--- src/zkafka/zkafka.py
+++ src/zkafka/zkafka.py
@@ -110,7 +110,8 @@ class Consumer(BaseClient):
                     
         return data
 
-    def get_data(self):
+    def get_data(self, value=False):
+        wait = 1
         while 1:
             msg = None
             try:
@@ -119,7 +120,8 @@ class Consumer(BaseClient):
                         self.commit(self.last_message)
                 elif self.config["raise.uncommitted"] and self.last_message:
                     raise Exception("Uncommited previous message")
-                if self.verbose:
+                if wait:
+                    wait = 0
                     print("Waiting for data...")
                 msg = self.client.poll(3)
             except SerializerError as e:
@@ -132,10 +134,16 @@ class Consumer(BaseClient):
             if msg is None:
                 continue
             elif not msg.error():
-                return msg
+                wait = 1
+                if value:
+                    return msg.value()
+                else:
+                    return msg
             elif msg.error().code() == KafkaError._PARTITION_EOF:
+                wait = 1
                 pass
             else:
+                wait = 1
                 traceback.print_exc()
                 bugsnag.notify(msg.error())
 
