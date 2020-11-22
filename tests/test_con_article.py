a7d39cc4eb8ee8c5cc01d27ea0510a85dfb286e0
Alfred Ray Jayag <jayagalray@gmail.com>
2020-11-22 13:17:50 +0400

    consumer no schema

--- tests/test_con_article.py
+++ tests/test_con_article.py
@@ -18,9 +18,11 @@ while 1:
     try:
         x = con.get_data()
         sleep = random.randint(1,3)
-        article = x.value()
+        # article = x.value()
         print("#####"*3)
-        print(">>", x.value()['title'],"\n", x.partition(), "  sleep:", sleep)
+        # print(x.value())
+        article = con.unpack(x.value())
+        print(">>", article['title'],"\n", x.partition(), "  sleep:", sleep)
         print(article.keys())
         for key in keys:
             if key in article:
