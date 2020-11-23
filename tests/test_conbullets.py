e0264562d7805ccafda99608bc9c8128f1f92a72
Alfred Ray Jayag <jayagalray@gmail.com>
2020-11-23 16:23:47 +0400

    nested testing

---
+++ tests/test_conbullets.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
+from zkafka import Consumer
+import sys
+import time
+import random
+from pprint import pprint
+
+topic = sys.argv[1]
+keys = ["bullets"]
+try:
+    keys += sys.argv[2:]
+except:
+    pass
+
+con = Consumer(topic, client_id="con-1", group_id="art_group")
+while 1:
+    try:
+        x = con.get_data()
+        sleep = random.randint(1,3)
+        # article = x.value()
+        print("#####"*3)
+        # print(x.value())
+        article = con.unpack(x.value())
+        print(">>", article['title'],"\n", x.partition(), "  sleep:", sleep)
+        print(article.keys())
+        for key in keys:
+            if key in article:
+                print(key, " >> ")
+                pprint(article[key])
+        print("#####"*3)
+        time.sleep(sleep)
+        con.commit(x)
+    except KeyboardInterrupt:
+        break
---
+++ tests/test_prodbullets.py
@@ -0,0 +1,41 @@
+import os
+import sys
+_root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src")
+print("ROOT", _root)
+sys.path.insert(0, _root)
+import traceback
+from zkafka import Producer
+from pymongo import MongoClient
+import re
+
+mongo = MongoClient()
+db = mongo.articles_db
+articles = db.articles
+
+total = 1
+pipe = "6"
+topic = "prod_pipe"
+if len(sys.argv) > 1:
+    try:
+        print(sys.argv[1])
+        topic = sys.argv[1]
+        pipe = sys.argv[2]
+        total = int(sys.argv[3])
+    except:
+        traceback.print_exc()
+prod = Producer(topic)
+
+print(">>", topic, pipe)
+for art in articles.aggregate([ { '$match': { 'bullets': re.compile(r"^[\S]+$") } }, { '$sample': { 'size': total } } ]):
+    print(">>", art["_id"])
+    print(">>", art["title"])
+    print(art.keys())
+    try:
+        prod.send_data(art)
+    except Exception as e:
+        print("\n\n")
+        print("!!!", art["_id"])
+        print("\n\n")
+        raise e
+prod.flush()
+print("Data sent")
