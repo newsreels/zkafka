e5db47a903ea9b8b31fac6335bc2d75e167d86a9
Shreyesh <76511222+shreyesh-ziro@users.noreply.github.com>
2021-12-05 17:51:02 +0400

    Update clients.py

--- src/zkafka/avro/clients.py
+++ src/zkafka/avro/clients.py
@@ -57,13 +57,13 @@ class BaseClient:
             "bootstrap.servers": os.getenv("KAFKA_BROKERS"),
         }
         if os.getenv("KAFKA_USE_SSL"):
-            settings.update({
-                "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
-                "sasl.mechanism": "PLAIN",
-                "ssl.ca.location": os.getenv("KAFKA_CERT_FILEPATH") or "probe", #/usr/local/etc/openssl/cert.pem
+            # settings.update({
+            #     "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
+            #     "sasl.mechanism": "PLAIN",
+            #     "ssl.ca.location": os.getenv("KAFKA_CERT_FILEPATH") or "probe", #/usr/local/etc/openssl/cert.pem
                 # "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
                 # "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
-            })
+            # })
         elif not os.getenv("KAFKA_USE_LOCAL"):
             settings.update({
                 "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
