e75a8a069b39dde3964354917ac47affc34e9a1c
Shreyesh <76511222+shreyesh-ziro@users.noreply.github.com>
2021-12-05 16:44:23 +0400

    default sasl username

--- src/zkafka/base/config.py
+++ src/zkafka/base/config.py
@@ -9,15 +9,15 @@ def config():
             "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
             "sasl.mechanism": "PLAIN",
             "ssl.ca.location": os.getenv("KAFKA_CERT_FILEPATH") or "probe", #/usr/local/etc/openssl/cert.pem
-            "sasl.username": os.getenv("KAFKA_API_KEY"), #<api-key>
-            "sasl.password": os.getenv("KAFKA_API_SECRET"), #<api-secret>
+            "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
+            "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
         })
     elif not os.getenv("KAFKA_USE_LOCAL"):
         settings.update({
             "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
             "sasl.mechanism": os.getenv("KAFKA_SASL_MECHANISM") or "PLAIN",
-            "sasl.username": os.getenv("KAFKA_API_KEY"), #<api-key>
-            "sasl.password": os.getenv("KAFKA_API_SECRET"), #<api-secret>
+            "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
+            "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
         })
     
     return settings
