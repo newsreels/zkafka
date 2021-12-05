bcffcb63364de6ebfe8cb1719efa6ade0a5fa59e
Shreyesh <76511222+shreyesh-ziro@users.noreply.github.com>
2021-12-05 17:34:29 +0400

    Update clients.py

--- src/zkafka/avro/clients.py
+++ src/zkafka/avro/clients.py
@@ -51,7 +51,7 @@ class BaseClient:
             if os.getenv("KAFKA_SCHEMA_API_KEY") and os.getenv("KAFKA_SCHEMA_API_SECRET"):
                 schema_settings.update({"basic.auth.user.info": os.getenv("KAFKA_SCHEMA_API_KEY")+":"+os.getenv("KAFKA_SCHEMA_API_SECRET")}) #<api-key>:<api-secret>
             else:
-                schema_settings.update({"basic.auth.user.info": ''})
+                schema_settings.update({"basic.auth.user.info": ':'})
 
         settings = {
             "bootstrap.servers": os.getenv("KAFKA_BROKERS"),
@@ -61,15 +61,15 @@ class BaseClient:
                 "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
                 "sasl.mechanism": "PLAIN",
                 "ssl.ca.location": os.getenv("KAFKA_CERT_FILEPATH") or "probe", #/usr/local/etc/openssl/cert.pem
-                "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
-                "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
+                # "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
+                # "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
             })
         elif not os.getenv("KAFKA_USE_LOCAL"):
             settings.update({
                 "security.protocol": os.getenv("KAFKA_SEC_PROTOCOL") or "SASL_SSL",
                 "sasl.mechanism": os.getenv("KAFKA_SASL_MECHANISM") or "PLAIN",
-                "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
-                "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
+                # "sasl.username": os.getenv("KAFKA_API_KEY") if os.getenv("KAFKA_API_KEY") else '', #<api-key>
+                # "sasl.password": os.getenv("KAFKA_API_SECRET")if os.getenv("KAFKA_API_SECRET") else '', #<api-secret>
             })
 
         self._schema_str = schema_str
